424dc5b8cdefedb41d9760289162ef406eccf408
Kjubo <yining159159@163.com>
2013-11-19 13:04:26 +0800

    添加WCF项目

---
+++ LS.Host/LSHostService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+using XMS.Core;
+using XMS.Core.WCF;
+
+namespace LS.Host
+{
+    public partial class LSHostService : ServiceBase
+    {
+        public LSHostService()
+        {
+            InitializeComponent();
+
+            // 向服务管理器中注册服务
+            //ManageableServiceHostManager.Instance.RegisterService(typeof(RestZPCYService));
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // 启动服务管理器
+            ManageableServiceHostManager.Instance.Start();
+
+            // 启动任务管理器
+            // TaskManager.Instance.Start();
+
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            // 停止服务管理器
+            ManageableServiceHostManager.Instance.Stop();
+
+            // 停止任务管理器
+            // TaskManager.Instance.Stop();
+
+            base.OnStop();
+        }
+    }
+}
