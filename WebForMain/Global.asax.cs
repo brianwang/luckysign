27a94fcc494aa3501c22fb8999f79ca11ded0989
Shangdl <neptune164.8@hotmail.com>
2013-12-11 18:02:53 +0800

    aaaa

---
+++ WebForMain/Global.asax.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Routing;
+
+namespace WebForMain
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute("发问题",
+                "Quest/Talk",
+                "~/Quest/Talk.aspx",
+                false);
+            routes.MapPageRoute("发新帖",
+                "Quest/Ask",
+                "~/Quest/Ask.aspx",
+                false);
+            routes.MapPageRoute("煮酒论命问答列表",
+                "Quest/QuestList/{cate}/{pn}",
+                "~/Quest/QuestList.aspx",
+                false,
+                new RouteValueDictionary { { "pn", "1" } },
+                new RouteValueDictionary { { "cate", @"\d*" }, { "pn", @"\d*" } }
+            );
+
+        }
+
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
