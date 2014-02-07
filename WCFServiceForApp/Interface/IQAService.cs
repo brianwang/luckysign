7e47725b347b3f294527d3c13d6a8a521632d362
Shangdl <neptune164.8@hotmail.com>
2014-02-07 18:01:22 +0800

    aaaa

---
+++ WCFServiceForApp/Interface/IQAService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ComponentModel;
+
+using XMS.Core;
+
+using AppMod.User;
+using AppDal.User;
+
+namespace WCFServiceForApp
+{
+    [ServiceContract(Namespace = "http://api.ssqian.com/QA")]
+    public interface IQAService
+    {
+        [OperationContract, WebGet(UriTemplate = "/login?uname={username}&pwd={password}")]
+        [Description("登录,/login?uname={username}&pwd={password}")]
+        ReturnValue<USR_CustomerMod> UserLogin(string username, string password);
+
+        
+    }
+
+}
