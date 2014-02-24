ab5ba344d52ad2ad4a37ac7f0c7e40523f5b7a89
Shangdl <neptune164.8@hotmail.com>
2014-02-24 09:55:20 +0800

    aaaa

--- WCFHost/Properties/AssemblyInfo.cs
+++ WCFHost/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("WebServiceForApp")]
+[assembly: AssemblyTitle("WCFHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WebServiceForApp")]
+[assembly: AssemblyProduct("WCFHost")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("7912dcb9-1988-4d00-a8ca-2591681dd261")]
+[assembly: Guid("ac101f73-4d2c-4d28-8efa-3ee36665dda3")]
 
 // 程序集的版本信息由下面四个值组成:
 //
---
+++ WCFHost/Service.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using XMS.Core;
+using XMS.Core.Logging;
+using XMS.Core.Configuration;
+using XMS.Core.WCF;
+using XMS.Core.Tasks;
+
+namespace WCFHost
+{
+    public partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+
+            ManageableServiceHostManager.Instance.RegisterService(typeof(WebServiceForApp.SSQianService));
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ManageableServiceHostManager.Instance.Start();
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            ManageableServiceHostManager.Instance.Stop();
+            base.OnStop();
+        }
+    }
+}
