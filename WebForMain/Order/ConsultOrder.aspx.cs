e4d0d1c635cfffc19bb3a54996fe2f98687bb706
ShangDL <neptune164.8@hotmail.com>
2014-08-18 00:02:07 +0800

    aaa

---
+++ WebForMain/Order/ConsultOrder.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AppBll.Order;
+using AppMod.Order;
+
+namespace WebForMain.Order
+{
+    public partial class ConsultOrder : PageBase
+    {
+        public string orderid = "";
+        public string price = "";
+        private int ordersysno = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Login(Request.Url.ToString());
+            if (Request.QueryString["order"] != null)
+            {
+                try
+                {
+                    ordersysno = int.Parse(Request.QueryString["order"]);
+                }
+                catch
+                {
+                    ShowError("");
+                }
+            }
+            else
+            {
+                ShowError("");
+            }
+            ORD_CashMod m_order = ORD_CashBll.GetInstance().GetModel(ordersysno);
+            orderid = m_order.OrderID;
+            price = m_order.PayAmount.ToString("￥0.00");
+
+            PayPannel1.orderSysNo = ordersysno;
+            PayPannel1.orderID = orderid;
+            PayPannel1.orderType = 1;
+            PayPannel1.initial();
+        }
+    }
+}
---
+++ WebForMain/Order/PayResult.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/Main.Master" AutoEventWireup="true" CodeBehind="PayResult.aspx.cs" Inherits="WebForMain.Order.PayResult" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="bottom" runat="server">
+</asp:Content>
